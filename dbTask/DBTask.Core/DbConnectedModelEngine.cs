59b144428549c0a81375e69c13ba483e32879ca6
Stefan Zhekov <stefan.z@sbtech.comgit config --global user.name Stefan>
2018-06-21 08:44:50 +0300

    init

---
+++ dbTask/DBTask.Core/DbConnectedModelEngine.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBTask.Core
+{
+    public class DbConnectedModelEngine
+    {
+        public DbConnectedModelEngine(IDbConnection connection, IDbCommand command)
+        {
+            this.Connection = connection;
+            this.Command = command;
+        }
+
+        public IDbConnection Connection { get; private set; }
+        public IDbCommand Command { get; set; }
+
+        public void ConfigureCommand(string text, CommandType type, IDbConnection connection)
+        {
+            using (this.Command.Connection)
+            {
+                this.Command.Connection = connection;
+                this.Command.Connection.Open();
+                this.Command.CommandText = text;
+                this.Command.CommandType = type;
+                this.Command.ExecuteNonQuery();
+
+            };
+        }
+        private void ConfigConnectionString()
+        {
+            this.Connection.ConnectionString = "Data Source=.\\SQLEXPRESS;Initial Catalog = ChefichaDB;Integrated Security=SSPI;";
+
+        }
+        private void AddParametersToACommand(int id, string nameS, string dateOfStart, bool isActive)
+        {
+            SqlParameter eventId = new SqlParameter("@ID", SqlDbType.Int, 255, "EventiD");
+            eventId.Value = id;
+            SqlParameter name = new SqlParameter("@Name", SqlDbType.NVarChar, 50, "Name");
+            name.Value = nameS;
+            SqlParameter date = new SqlParameter("@StartingDate", SqlDbType.DateTime, 50, "StartingDate");
+            date.Value = dateOfStart;
+            SqlParameter IsActive = new SqlParameter("@IsActive", SqlDbType.Bit, 1, "IsActive");
+            IsActive.Value = isActive;
+            this.Command.Parameters.Add(eventId);
+            this.Command.Parameters.Add(name);
+            this.Command.Parameters.Add(date);
+            this.Command.Parameters.Add(IsActive);
+        }
+        public void Run()
+        {
+
+        }
+
+       
+    }
+}
---
+++ dbTask/DBTask.Core/Injector.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Practices.Unity.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+
+namespace DBTask.Core
+{
+    class Injector
+
+    { 
+        private IUnityContainer container;
+    private static Injector Instance = null;
+    private static readonly object lockObj = new object();
+    private Injector() : this(new UnityContainer())
+    {
+
+    }
+    private Injector(IUnityContainer container)
+    {
+        var sectionconfig = (UnityConfigurationSection)ConfigurationManager.GetSection("unity");
+        if (sectionconfig != null)
+        {
+            sectionconfig.Configure(this.container);
+        }
+        this.container = container;
+
+    }
+
+    public static Injector GetInstance()
+    {
+        if (Instance == null)
+        {
+
+
+            lock (lockObj)
+            {
+                if (Instance == null)
+                {
+                    Instance = new Injector();
+                }
+            }
+
+        }
+        return Instance;
+    }
+    public T Resolve<T>()
+    {
+        T ret = default(T);
+
+        if (this.container.IsRegistered(typeof(T)))
+        {
+            ret = this.container.Resolve<T>();
+        }
+        return ret;
+    }
+    public void RegisterType<T>(T instance)
+    {
+        this.container.RegisterInstance<T>(instance);
+    }
+}
+}
